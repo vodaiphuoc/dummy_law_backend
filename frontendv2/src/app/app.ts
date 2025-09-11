cacacb83157790cf8b073fdd9e5a1d2daba341c8
vodaiphuoc <phuocvodn98@gmail.com>
2025-09-11 17:08:25 +0700

    oke

--- frontendv2/src/app/app.ts
+++ frontendv2/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Conversations } from './conversations/conversations';
+import { MessageArea } from './message-area/message-area';
+import { MessageInput } from './message-input/message-input';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Conversations, MessageArea, MessageInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- frontendv2/src/app/message-input/message-input.ts
+++ frontendv2/src/app/message-input/message-input.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-message-input',
-  imports: [],
-  templateUrl: './message-input.html',
-  styleUrl: './message-input.css'
+    selector: 'app-message-input',
+    imports: [],
+    templateUrl: './message-input.html',
+    styleUrl: './message-input.css'
 })
 export class MessageInput {
 
